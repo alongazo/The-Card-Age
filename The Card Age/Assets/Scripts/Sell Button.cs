13e885a08ddea1ad34019cbe5c4905ef485a9436
alongazo <alexlongazo@gmail.com>
2016-10-07 11:34:11 -0700

    Created a basic model for the in-game shop

---
+++ The Card Age/Assets/Scripts/Sell Button.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Camera.main.transform.Translate((float)-0.882, (float)5.068, (float)-0.277);
+	}
+}
