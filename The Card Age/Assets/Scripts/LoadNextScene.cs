0d67be2b0d7b7c9ab5b61623c82b35e3b0354d26
alongazo <alexlongazo@gmail.com>
2016-10-09 13:17:11 -0700

    Added a fade in to the title screen, along with a blinking "Click to Play" button that takes you to the shop

---
+++ The Card Age/Assets/Scripts/LoadNextScene.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+public class LoadNextScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("Shop");
+    }
+}
--- The Card Age/Assets/Scripts/TitleScreenFadeIn.cs
+++ The Card Age/Assets/Scripts/TitleScreenFadeIn.cs
@@ -2,13 +2,29 @@
 using System.Collections;
 
 public class TitleScreenFadeIn : MonoBehaviour {
-    
+    public bool increaseFade = false;
 	// Use this for initialization
 	void Start () {
-
+        Wait(5);
+        increaseFade = true;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	}
+        Color color = transform.GetComponent<Renderer>().material.color;
+        if (!increaseFade)
+        {
+            color.a -= .03f * Time.deltaTime;
+        }
+        else
+        {
+            color.a -= .1f * Time.deltaTime;
+        }
+        transform.GetComponent<Renderer>().material.color = color;
+
+    }
+    IEnumerator Wait(float time)
+    {
+        yield return new WaitForSeconds(time);
+    }
 }
