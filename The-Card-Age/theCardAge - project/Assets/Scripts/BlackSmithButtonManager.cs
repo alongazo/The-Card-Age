be9b83c1631076ffaf43033a42c701bdc8a4d2fb
alongazo <alexlongazo@gmail.com>
2016-12-05 19:59:54 -0800

    Added new sound effects and the credits scene in the village

---
+++ The-Card-Age/theCardAge - project/Assets/Scripts/BlackSmithButtonManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class BlackSmithButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.Find("SettingTracker").GetComponent<TrackSetting>().currentSetting == "blacksmith")
+        {
+            transform.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            transform.GetComponent<Button>().interactable = false;
+        }
+    }
+}
---
+++ The-Card-Age/theCardAge - project/Assets/Scripts/ShopBuyButtonManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShopBuyButtonManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameObject.Find("SettingTracker").GetComponent<TrackSetting>().currentSetting == "shop-buy")
+        {
+            transform.GetComponent<Button>().interactable = true;
+        }
+        else
+        {
+            transform.GetComponent<Button>().interactable = false;
+        }
+    }
+}
